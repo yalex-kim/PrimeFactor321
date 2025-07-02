bac6374eb8333f77b0a506a76142f4cf1f6c6743
yalex-kim <yunbae.alex.kim@gmail.com>
2025-07-02 10:18:08 +0900

    feature/of9 - refactor2

--- PrimeFactor/prime-factors.cpp
+++ PrimeFactor/prime-factors.cpp
@@ -9,14 +9,7 @@ class PrimeFactors {
 		if (number > 1)
 		{
 			int divisor = 2;
-			if (number == 4) {
-				while (number % divisor == 0) {
-					factors.push_back(divisor);
-					number /= divisor;
-				}
-				
-			}
-			else if (number == 6 || number == 9) {
+			if (number == 4 || number == 6 || number == 9) {
 				for (divisor = 2;  number > 1 ; divisor++) {
 					while (number % divisor == 0) {
 						factors.push_back(divisor);
