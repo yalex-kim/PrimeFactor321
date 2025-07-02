f0b299dee032eea027548d6946b0c7aebf420121
yalex-kim <yunbae.alex.kim@gmail.com>
2025-07-02 10:18:53 +0900

    feature/of12

--- PrimeFactor/prime-factors_test.cpp
+++ PrimeFactor/prime-factors_test.cpp
@@ -39,3 +39,7 @@ TEST_F(PrimeFactorsTest, Of9) {
 	expected = {3, 3 };
 	EXPECT_EQ(expected, prime_factor.of(9));
 }
+TEST_F(PrimeFactorsTest, Of12) {
+	expected = {2,2,3 };
+	EXPECT_EQ(expected, prime_factor.of(12));
+}
