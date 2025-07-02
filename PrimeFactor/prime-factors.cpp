53ca86c6f345c3f0fe0d39b48ece5489ba1116b0
yalex-kim <yunbae.alex.kim@gmail.com>
2025-07-02 10:19:57 +0900

    feature/of12 - refactoring

--- PrimeFactor/prime-factors.cpp
+++ PrimeFactor/prime-factors.cpp
@@ -6,22 +6,13 @@ class PrimeFactors {
 	public:
 	vector<int> of(int number) {
 		vector<int> factors;
-		if (number > 1)
-		{
-			int divisor = 2;
-			if (number == 4 || number == 6 || number == 9 || number == 12) {
-				for (divisor = 2;  number > 1 ; divisor++) {
-					while (number % divisor == 0) {
-						factors.push_back(divisor);
-						number /= divisor;
-					}
-				}
-			}
-			else {
-				factors.push_back(number);
+		
+		for (int divisor = 2;  number > 1 ; divisor++) {
+			while (number % divisor == 0) {
+				factors.push_back(divisor);
+				number /= divisor;
 			}
 		}
-		
 
 		return factors;
 	}
